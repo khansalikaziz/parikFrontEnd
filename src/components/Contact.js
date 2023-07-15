ba3856018474e8234cb659a2c30816c4a6944c1b
khansalikaziz <khansalikaziz786@gmail.com>
2023-07-15 17:23:14 +0530

    fn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
+
 export const Contact = () => {
   const formInitialDetails = {
     firstName: '',
